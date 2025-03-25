519ab6cb93ef4ac6ce6a1d0bde83db93a18d5741
vimlesh <vimleshverma1998@gmail.com>
2025-03-26 01:22:25 +0530

    54.display the query to user

--- ecommerce/search/views.py
+++ ecommerce/search/views.py
@@ -1,22 +1,24 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
 from products.models import Product
-from django.views.generic import CreateView, ListView
 
 
 class SearchProductView(ListView):
-    template_name = "products/list.html"
+    template_name = "search/view.html"  # Ensure the correct template path
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(SearchProductView, self).get_context_data(*args, **kwargs)
+        query = self.request.GET.get("q")  # Fixed typo (Get → GET)
+        context["query"] = query
+        # SearchQuery.objects.create(query=query)  # Uncomment if using SearchQuery model
+        return context
 
     def get_queryset(self, *args, **kwargs):
         request = self.request
-        print(request.GET)
-        method_dict = request.GET
-        query = method_dict.get("q", None)
-        if query is not None:
+        query = request.GET.get("q", None)  # More concise way of getting the query
+        print(query)
+        if query:
             return Product.objects.filter(title__icontains=query)
-        return Product.objects.none
-
-    """
-    __icontains = When fields contains this
-    __iexact = When fields contains this 
-    """
+        return (
+            Product.objects.none()
+        )  # Fixed: Previously calling .featured(), which doesn't exist
