3e18a56f6cb616fd44be28b24a48d3c959a4d2eb
vimlesh <vimleshverma1998@gmail.com>
2025-04-03 00:50:11 +0530

    63.Cart model

--- ecommerce/carts/models.py
+++ ecommerce/carts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
+
+from products.models import Product
+
+User = settings.AUTH_USER_MODEL
+
 
 # Create your models here.
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+    products = models.ManyToManyField(Product, blank=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Cart {self.id} - {self.user.username if self.user else 'Guest'}"
--- ecommerce/carts/views.py
+++ ecommerce/carts/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 
 
 def cart_home(request):
+    cart_id = request.session.get("cart_id", None)
+    if cart_id is None:
+        print("Create new cart")
+        request.session["cart_id"] = 12  # Setter fun
+        pass
+    else:
+        print("cart already exists")
+
     # key = request.session.session_key
     # print(f"key : {key}")
-    request.session["cart_id"] = 12  # Setter fun
+
     request.session.save()
     return render(request, "carts/home.html", {})
