7e0c809d577994d4246615496c107edc5e8c7b92
vimlesh <vimleshverma1998@gmail.com>
2025-04-03 23:53:58 +0530

    67.m2m_changed signal to calculate cart total

--- ecommerce/carts/models.py
+++ ecommerce/carts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 
 from products.models import Product
+from django.db.models.signals import pre_save, post_save, m2m_changed
 
 User = settings.AUTH_USER_MODEL
 
@@ -36,6 +37,7 @@ class Cart(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
     products = models.ManyToManyField(Product, blank=True)
     total = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    subtotal = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
@@ -43,3 +45,28 @@ class Cart(models.Model):
 
     def __str__(self):
         return f"Cart {self.id} - {self.user.username if self.user else 'Guest'}"
+
+
+def m2m_changed_cart_receiver(sender, instance, action, *args, **kwargs):
+    # print(action)
+    # print(instance.total)
+    # print(instance.products.all())
+    if action == "post_add" or action == "post_clear" or action == "post_remove":
+        products = instance.products.all()
+        total = 0
+        for product in products:
+            total += product.price
+        # print(total)
+        if instance.subtotal != total:
+            instance.subtotal = total
+            instance.save()
+
+
+m2m_changed.connect(m2m_changed_cart_receiver, sender=Cart.products.through)
+
+
+def pre_save_cart_receiver(sender, instance, action, *args, **kwargs):
+    instance.total = instance.subtotal
+
+
+pre_save.connect(pre_save_cart_receiver, sender=Cart)
--- ecommerce/carts/views.py
+++ ecommerce/carts/views.py
@@ -2,12 +2,6 @@ from django.shortcuts import render
 from .models import Cart
 
 
-def create_cart(user=None):
-    cart_obj = Cart.objects.create(user=None)
-    print("New Cart Created")
-    return cart_obj
-
-
 def cart_home(request):
     cart_obj, obj = Cart.objects.new_or_get(request)
     return render(request, "carts/home.html", {})
