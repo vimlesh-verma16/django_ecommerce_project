3c185b1f747c8796a87e39d4da9831d8d6e8af1a
vimlesh <vimleshverma1998@gmail.com>
2025-05-20 16:24:55 +0530

    65. Cart model manager part 1

--- ecommerce/carts/models.py
+++ ecommerce/carts/models.py
@@ -7,12 +7,23 @@ User = settings.AUTH_USER_MODEL
 
 
 # Create your models here.
+class CartManager(models.Manager):
+    def new(self, user=None):
+        user_obj = None
+        if user is not None:
+            if user.is_authenticated:
+                user_obj = user
+        return self.model.objects.create(user=user_obj)
+
+
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     products = models.ManyToManyField(Product, blank=True)
     total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    objects = CartManager()
 
     def __str__(self):
         return str(self.id)
